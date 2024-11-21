7a5d10db563706b82b5863d8784fae9fbc541947
Cristhian-06 <bospro09@outlook.com>
2024-11-21 14:04:28 -0600

    proyecto we

---
+++ html shi/Proyecto final/js.js
@@ -0,0 +1,10 @@
+asientos=[]
+function add(){
+    for(let i=1;i>=24;i++){
+        asientos=document.getElementById(box1),value;
+        let nom=document.getElementById(box2),value;
+        if("res" +asientos){
+            
+        }
+    }
+    }
